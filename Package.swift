e6b9342cc4adae2f461f9b3ad61278038af17b4e
GaborVass <gabor.vass@gmail.com>
2017-12-18 21:16:58 +0100

    build

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0"))
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/tid-kijyun/Kanna.git", .branch("feature/v4.0.0")),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor", "FluentProvider"],
