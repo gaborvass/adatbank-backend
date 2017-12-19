009ddf28435f1dae650f16c0bab4817949193588
GaborVass <gabor.vass@gmail.com>
2017-12-19 22:40:00 +0100

    standings rounds leagues

--- Sources/App/Setup/Implementations/DefaultDataParser.swift
+++ Sources/App/Setup/Implementations/DefaultDataParser.swift
@@ -16,10 +16,10 @@ class DefaultDataParser : DataParserProtocol {
         var retValue: Array<MatchObject> = Array<MatchObject>()
         let doc = try! HTML(html: raw, encoding: String.Encoding.utf8)
         // must be only one
-        for link in doc.xpath("//*[@id=\"matchContent\"]") {
+        for link in doc.xpath("//*[@id=\"schedule_box\"]") {
             let innerHtml = link.innerHTML
             let matchesTable = try! HTML(html: innerHtml!, encoding: String.Encoding.utf8)
-            for matchItem in matchesTable.xpath("//*[@class=\"matchItem\"]") {
+            for matchItem in matchesTable.xpath("//*[@class=\"schedule\"]") {
                 retValue.append(parseMatchItem(matchItem))
             }
         }
@@ -28,7 +28,7 @@ class DefaultDataParser : DataParserProtocol {
     
     func parseMatchItem(_ item: Kanna.XMLElement) -> MatchObject {
         let matchId : String = item["rel"]!
-        let mo = MatchObject(matchId)
+        let mo = MatchObject("")
         
         let innerHtml = item.innerHTML
         let matchItemHTML = try! HTML(html: innerHtml!, encoding: String.Encoding.utf8)
@@ -134,37 +134,41 @@ class DefaultDataParser : DataParserProtocol {
     
     func parseLeagues(_ raw: String) -> Array<LeagueObject> {
         var retValue: Array<LeagueObject> = Array<LeagueObject>()
-//        let json = SwiftyJSON.parse(raw)
-//        let leagues = json["leagues"].arrayValue
-//        for i in 0 ..< leagues.count {
-//            let id = leagues[i]["id"].stringValue
-//            let name = leagues[i]["name"].stringValue
-//
-//            let league: LeagueObject = LeagueObject(id)
-//            league.leagueName = name
-//
-//            retValue.append(league)
-//        }
+        let json : [String: Any] =  (try! JSONSerialization.jsonObject(with: raw.data(using: .utf8)!, options: .allowFragments) as? [String: Any])!
+        if let leagues = json["leagues"] as? Array<Dictionary<String, String>> {
+            for i in 0 ..< leagues.count {
+                let id = leagues[i]["id"]
+                let name = leagues[i]["name"]
+                
+                let league: LeagueObject = LeagueObject(id!)
+                league.leagueName = name
+                
+                retValue.append(league)
+            }
+        }
         return retValue;
     }
     
     func parseRounds(_ raw: String) -> Array<RoundObject> {
         var retValue: Array<RoundObject> = Array<RoundObject>()
-//        let json = SwiftyJSON.parse(raw)
-//        let rounds = json["turns"].arrayValue
-//        for i in 0 ..< rounds.count {
-//            let id = rounds[i]["turn"].string
-//            
-//            let ro: RoundObject = RoundObject(id!)
-//            ro.roundName = id
-//            ro.isCurrent = false
-//            retValue.append(ro)
-//        }
-//        let actualTurn = json["actualTurn"]["turn"].string
-//        if actualTurn != nil && actualTurn!.count > 0 {
-//            let actualTurnInt = Int.init(actualTurn!)!
-//            retValue[actualTurnInt - 1].isCurrent = true
-//        }
+        let json : [String: Any] =  (try! JSONSerialization.jsonObject(with: raw.data(using: .utf8)!, options: .allowFragments) as? [String: Any])!
+        
+        if let rounds : Array<Dictionary<String, String>> = json["turns"] as? Array<Dictionary<String, String>>  {
+            for i in 0 ..< rounds.count {
+                let id = rounds[i]["turn"]!.string
+                
+                let ro: RoundObject = RoundObject(id)
+                ro.roundName = id
+                ro.isCurrent = false
+                retValue.append(ro)
+            }
+        }
+        if let actualTurn = json["actualTurn"] as? Dictionary<String, String> {
+            if let turnId = actualTurn["turn"] {
+                let actualTurnInt = Int.init(turnId)!
+                retValue[actualTurnInt - 1].isCurrent = true
+            }
+        }
         return retValue
     }
     
@@ -241,7 +245,9 @@ class DefaultDataParser : DataParserProtocol {
             let innerHtml = standings.innerHTML
             let standingsHTML = try! HTML(html: innerHtml!, encoding: String.Encoding.utf8)
             for standingsRow in standingsHTML.xpath("//*[@class=\"template-tr-selectable noNb1Tr\"]") {
-                let to = TeamObject("")
+                let clickRef = standingsRow["onclick"]
+                let teamId = clickRef?.split(separator: "/").last?.split(separator: ".").first
+                let to = TeamObject(String(teamId!))
                 let standingsRowHtml = standingsRow.innerHTML
                 let standingsRowItem = try! HTML(html: standingsRowHtml!, encoding: String.Encoding.utf8)
                 var counter : Int = 0
@@ -249,31 +255,31 @@ class DefaultDataParser : DataParserProtocol {
                     if counter == 0 {
                         to.position = standingsRowColumn.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
                     }
-                    if counter == 1 {
+                    if counter == 2 {
                         to.teamName = standingsRowColumn.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
                     }
-                    if counter == 2 {
+                    if counter == 3 {
                         to.played = standingsRowColumn.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
                     }
-                    if counter == 3 {
+                    if counter == 4 {
                         to.won = standingsRowColumn.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
                     }
-                    if counter == 4 {
+                    if counter == 5 {
                         to.drawn = standingsRowColumn.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
                     }
-                    if counter == 5 {
+                    if counter == 6 {
                         to.lost = standingsRowColumn.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
                     }
-                    if counter == 6 {
+                    if counter == 7 {
                         to.goalsFor = standingsRowColumn.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
                     }
-                    if counter == 7 {
+                    if counter == 8 {
                         to.goalsAgainst = standingsRowColumn.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
                     }
-                    if counter == 8 {
+                    if counter == 9 {
                         to.goalDiff = standingsRowColumn.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
                     }
-                    if counter == 9 {
+                    if counter == 10 {
                         to.points = standingsRowColumn.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
                     }
                     counter = counter + 1
